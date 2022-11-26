65319f5e61cd83c4eeca6ee6ebd84686387892c1
pjpawel <pawel.jan.podgorski@gmail.com>
2022-11-26 11:44:26 +0100

    Change test name

--- tests/MagisBundleTest.php
+++ tests/MagisBundleTest.php
@@ -3,13 +3,39 @@
 namespace pjpawel\Magis\MagisBundle\Test;
 
 use PHPUnit\Framework\TestCase;
+use pjpawel\Magis\MagisBundle\MagisBundle;
+use pjpawel\Magis\ViewDispatcherService;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class MagisBundleTest extends TestCase
 {
 
-    public function testFuture(): void
+    public function testServiceWiring()
     {
-        $this->assertTrue(true);
-        // This will be future test
+
+        $kernel = new MagisKernel('test', true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        $magis = $container->get('magis.view');
+        $this->assertInstanceOf(ViewDispatcherService::class, $magis);
+        $this->assertIsString($magis->getDefaultViewClass());
+    }
+}
+
+class MagisKernel extends Kernel
+{
+
+    public function registerBundles(): iterable
+    {
+        return [
+            new MagisBundle()
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(realpath(__DIR__ . '/../recipes/config/packages/magis.yaml'));
     }
 }
